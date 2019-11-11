9e74f9920e3f240463c67830181ed7867db4c79a
Mangesh Patankar <mdpatankar@gmail.com>
2019-11-11 21:11:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Welcome to IBM Cloud Paks Verify!");
+  res.send("Hello from Appsody Welcome to IBM Cloud Paks Verify! test");
 });
  
 module.exports.app = app;
