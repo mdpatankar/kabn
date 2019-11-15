9701e2de25a79a5b7ce2f6852b1105be325c37e6
Mangesh <mdpatankar@gmail.com>
2019-11-15 12:58:55 +0530

    new16nov

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Welcome to IBM Cloud Paks Verify!!!!");
+  res.send("Hello from Appsody Demo! to Hello from Cloud Paks !!!")
 });
  
 module.exports.app = app;
