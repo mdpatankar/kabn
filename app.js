47cf7ef9f8963553b26ddead28fff154e161928e
Mangesh Patankar <mdpatankar@gmail.com>
2019-11-21 15:53:18 +0530

    newcommit
    newcommit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Demo! to Hello from Cloud Paks - Capgemini demo 19Nov !!!")
+  res.send("Hello from Appsody Demo! to Hello from Cloud Paks - Capgemini demo 2 was 1Nov !!!")
 });
  
 module.exports.app = app;
