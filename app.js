69baab893bfb3a02db2590ffeb16cf4c4480d175
Mangesh <mdpatankar@gmail.com>
2019-11-27 16:22:58 +0530

    newchange2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Demo! to Hello from Cloud Paks 27 Nov bew!!!")
+  res.send("Hello from Appsody Demo! to Hello from Cloud Paks 27 Nov Wipro !!!")
 });
  
 module.exports.app = app;
