b7919ea2d90cade5d1220c4e1fa362456217d6ee
Mangesh <mdpatankar@gmail.com>
2019-12-05 00:44:48 +0530

    commit2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Demo! to Hello from Cloud Paks 1 Dec!!!")
+  res.send("Hello from Appsody Demo! to Hello from Cloud Paks Barclays!!!")
 });
  
 module.exports.app = app;
