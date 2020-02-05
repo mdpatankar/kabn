5b3a2ddc846f505424ba771a5f5524e628e5081c
Mangesh <mdpatankar@gmail.com>
2020-02-05 13:36:16 +0530

    new change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Kabanero Microservice!!!")
+  res.send("Hello Kabanero Microservice HCL Demo!!!")
 });
  
 module.exports.app = app;
