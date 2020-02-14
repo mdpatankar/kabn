5190a096ce951608503abb80c25d406bdba7103f
Mangesh <mdpatankar@gmail.com>
2020-02-14 14:55:11 +0530

    commit demo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Kabanero Microservice HCL Demo with Kabanero!!!")
+  res.send("Hello Kabanero Microservice HCL Demo with Kabanero Today!!!")
 });
  
 module.exports.app = app;
